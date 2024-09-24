c7cf9cde13ea88dff842bd6a7561ef1543d9e5b8
Nischey <Nischey5144@conestogac.on.ca>
2024-09-23 23:25:55 -0400

    PRE-RELEASE

--- Assignment 2/Source.cpp
+++ Assignment 2/Source.cpp
@@ -7,47 +7,74 @@
 struct STUDENT_DATA {
     std::string firstName;
     std::string lastName;
+    std::string email;
 };
 
-int main() 
+int main()
 {
-    // Opens the input file here
-    std::ifstream inputFile("StudentData.txt");
+#ifdef PRE_RELEASE
+    std::cout << "Running pre-release source code" << std::endl;
+    std::ifstream inputFile("StudentData_Emails.txt"); // Open pre-release file
+#else
+    std::cout << "Running standard source code" << std::endl;
+    std::ifstream inputFile("StudentData.txt"); // Open standard file
+#endif
+
+    // Check if the file opened correctly
     if (!inputFile.is_open()) {
         std::cerr << "Error opening file." << std::endl;
         return 1;
     }
 
-
     // Vector to store student data
     std::vector<STUDENT_DATA> students;
 
-    // this read data from the file.
+    // Read data from the file
     std::string line;
     while (std::getline(inputFile, line)) {
-        
+        // Skip empty lines
+        if (line.empty()) {
+            continue;
+        }
 
-        // finds the position of the space between first and the last names. 
-
-        size_t pos = line.find(' ');
-        if (pos != std::string::npos) {
+        // Find the position of the first comma
+        size_t commaPos1 = line.find(',');
+        if (commaPos1 != std::string::npos) {
             STUDENT_DATA student;
-            student.firstName = line.substr(0, pos);
-            student.lastName = line.substr(pos + 1);
-            students.push_back(student); 
+
+            // In pre-release mode, find the second comma for the email
+            size_t commaPos2 = line.find(',', commaPos1 + 1);
+            if (commaPos2 != std::string::npos) {
+                // Extract last name
+                student.lastName = line.substr(0, commaPos1);
+                // Extract first name
+                student.firstName = line.substr(commaPos1 + 1, commaPos2 - commaPos1 - 1);
+                // Extract email
+                student.email = line.substr(commaPos2 + 1);
+            }
+            else {
+                // If there’s no second comma, we can handle it accordingly
+                student.lastName = line.substr(0, commaPos1);
+                student.firstName = line.substr(commaPos1 + 1);
+                student.email = ""; // No email available
+            }
+
+            students.push_back(student);
         }
     }
 
-    // Print student data in the debug 
-#ifdef _DEBUG
-    std::cout << "Student Information:" << std::endl;
-    for (const auto& student : students)
-
-    {
-        std::cout << "First Name: " << student.firstName << ", Last Name: " << student.lastName << std::endl;
-    }
-#endif
     // Close the file
     inputFile.close();
-    return 1;
+
+#ifdef DEBUG
+    // Debug output to verify student data
+    std::cout << "Student Information:" << std::endl;
+    for (const auto& student : students) {
+        std::cout << "First Name: " << student.firstName
+            << " | Last Name: " << student.lastName
+            << " | Email: " << student.email << std::endl;
+    }
+#endif // DEBUG
+
+    return 0; // Indicate successful execution
 }
